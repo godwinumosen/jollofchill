19759367f66147c10fad4534cccb177cbd910bbf
Godwin Umosen <umosengodwin568@gmail.com>
2025-06-30 10:04:03 +0100

    Model and urls implemented

--- jollofandchill/admin.py
+++ jollofandchill/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
 # Register your models here.
+from . import models
+from .models import DeusMagnusMainPost, SecondDeusMagnusMainPicturePost,SubPicture_2
+from .models import SubPicture_1, VideoSubImage
+
+#The DeusMagnus main post model admin
+class DeusMagnusMainPostModelAdmin (admin.ModelAdmin):
+    prepopulated_fields = {'deus_magnus_slug': ('deus_magnus_title',)}
+    list_display = ['deus_magnus_status','deus_manus_video','deus_magnus_author']
+admin.site.register(DeusMagnusMainPost, DeusMagnusMainPostModelAdmin)
--- jollofandchill/models.py
+++ jollofandchill/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+from datetime import datetime, date
+
+#The first main video on deusmagnus website
+
+# The main model for Deus Magnus Model category
+class DeusMagnusMainPost(models.Model):
+    deus_magnus_title = models.CharField(max_length=255, blank=True, null=True)
+    deus_magnus_status = models.CharField(max_length=255, blank=True, null=True)
+    deus_magnus_description = models.TextField()
+    deus_magnus_slug = models.SlugField (max_length=255,blank=True, null=True)
+    deus_manus_video = models.FileField(upload_to='videos/') 
+    #thumbnail = models.ImageField(max_length=100, null=True, blank=True)
+    deus_magnus_publish_date = models.DateTimeField (auto_now_add= True)
+    deus_magnus_author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+       
+    class Meta:
+        ordering =['-deus_magnus_publish_date']
+    
+    def __str__(self):
+        return self.deus_magnus_title + ' | ' + str(self.deus_magnus_author)
+    
+    def get_absolute_url(self):
+        return reverse('home','sub_video_img_detail','sub_detail_video','sub_detail',
+                       'board_detail','blog_detail','last_detail','second_detail','detail')
+    
