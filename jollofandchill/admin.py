5bde80b3fde525e7584e65544b1d3603f67e9d9d
Godwin Umosen <umosengodwin568@gmail.com>
2025-06-30 14:47:58 +0100

    First migrations commit

--- jollofandchill/admin.py
+++ jollofandchill/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 # Register your models here.
 from . import models
-'''from .models import DeusMagnusMainPost, SecondDeusMagnusMainPicturePost,SubPicture_2
+from .models import JollofandChillHeroHead
 
 
 #The DeusMagnus main post model admin
-class DeusMagnusMainPostModelAdmin (admin.ModelAdmin):
-    prepopulated_fields = {'deus_magnus_slug': ('deus_magnus_title',)}
-    list_display = ['deus_magnus_status','deus_manus_video','deus_magnus_author']
-admin.site.register(DeusMagnusMainPost, DeusMagnusMainPostModelAdmin)'''
+class JollofandChillHeroHeadPostModelAdmin (admin.ModelAdmin):
+    prepopulated_fields = {'hero_slug': ('hero_title_head',)}
+    list_display = ['hero_description','hero_img','hero_author']
+admin.site.register(JollofandChillHeroHead, JollofandChillHeroHeadPostModelAdmin)
--- jollofandchill/models.py
+++ jollofandchill/models.py
@@ -6,25 +6,21 @@ from datetime import datetime, date
 
 #The first main video on deusmagnus website
 
-# The main model for Deus Magnus Model category
-class DeusMagnusMainPost(models.Model):
-    deus_magnus_title = models.CharField(max_length=255, blank=True, null=True)
-    deus_magnus_status = models.CharField(max_length=255, blank=True, null=True)
-    deus_magnus_description = models.TextField()
-    deus_magnus_slug = models.SlugField (max_length=255,blank=True, null=True)
-    deus_manus_video = models.FileField(upload_to='videos/') 
-    #thumbnail = models.ImageField(max_length=100, null=True, blank=True)
-    deus_magnus_publish_date = models.DateTimeField (auto_now_add= True)
-    deus_magnus_author = models.ForeignKey(User, on_delete=models.CASCADE)
+class JollofandChillHeroHead(models.Model):
+    hero_title_head = models.CharField(max_length=255, blank=True, null=True)
+    hero_title = models.CharField(max_length=255, blank=True, null=True)
+    hero_description = models.TextField()
+    hero_slug = models.SlugField (max_length=255,blank=True, null=True)
+    hero_img = models.ImageField(upload_to='hero_images/') 
+    hero_publish_date = models.DateTimeField (auto_now_add= True)
+    hero_author = models.ForeignKey(User, on_delete=models.CASCADE)
 
        
     class Meta:
-        ordering =['-deus_magnus_publish_date']
+        ordering =['-hero_publish_date']
     
     def __str__(self):
-        return self.deus_magnus_title + ' | ' + str(self.deus_magnus_author)
+        return self.hero_title_head + ' | ' + str(self.hero_author)
     
     def get_absolute_url(self):
-        return reverse('home','sub_video_img_detail','sub_detail_video','sub_detail',
-                       'board_detail','blog_detail','last_detail','second_detail','detail')
-    
+        return reverse('home')
