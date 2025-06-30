d0cdf416c617c76837afc3cac51b113a0db31b23
Godwin Umosen <umosengodwin568@gmail.com>
2025-06-30 15:26:26 +0100

    First migrations commit

--- jollofandchill/admin.py
+++ jollofandchill/admin.py
@@ -6,6 +6,6 @@ from .models import JollofandChillHeroHead
 
 #The DeusMagnus main post model admin
 class JollofandChillHeroHeadPostModelAdmin (admin.ModelAdmin):
-    prepopulated_fields = {'hero_slug': ('hero_title_head',)}
+    prepopulated_fields = {'hero_slug': ('hero_title',)}
     list_display = ['hero_description','hero_img','hero_author']
 admin.site.register(JollofandChillHeroHead, JollofandChillHeroHeadPostModelAdmin)
