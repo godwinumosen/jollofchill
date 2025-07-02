03681d913505b6317d3dcddb4ad29dc81f56d4aa
Godwin Umosen <umosengodwin568@gmail.com>
2025-07-02 13:08:45 +0100

    First migrations commit

--- jollofandchill/urls.py
+++ jollofandchill/urls.py
@@ -1,15 +1,15 @@
 
 from django.urls import path
 from . import views
-from .views import HomeView
+from .views import HomeView,JolloAndChillArticleDetailView
 #from .views import SubPictureDetailView,SubVideoDetailView
 
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('', HomeView.as_view(), name="home"),
     path('contact/', views.contact, name='contact'),
-    #path('home/', HomeView.as_view(), name='home'),
-    #path('article/<int:pk>/', ArticleDetailView.as_view(), name="detail"),
+    path('home/', HomeView.as_view(), name='home'),
+    path('article/<int:pk>/', JolloAndChillArticleDetailView.as_view(), name="detail"),
     #path('article2/<int:pk>/', SecondConstructionDetailViewArticleDetailView.as_view(), name="second_detail"),
 ]
 
